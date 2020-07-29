9633319c825528c3c5d6636bbeb492d95703bda4
Gunil <gunil.na1@gmail.com>
2020-07-29 11:21:09 +0200

    finished

--- Weather.js
+++ Weather.js
@@ -1,17 +1,100 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import PropTypes from "prop-types";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 
-export default function Weather({ temp }) {
+const weatherOptions = {
+  Thunderstorm: {
+    iconName: "weather-lightning",
+    gradient: ["#373B44", "#4286f4"],
+    title: "Cloudy",
+    subtitle: "that's cool",
+  },
+  Drizzle: {
+    iconName: "weather-partly-rainy",
+    gradient: ["#89F7FE", "#66A6FF"],
+    title: "Cloudy",
+    subtitle: "that's cool",
+  },
+  Rain: {
+    iconName: "weather-rainy",
+    gradient: ["#00C6FB", "#005BEA"],
+    title: "Cloudy",
+    subtitle: "that's cool",
+  },
+  Snow: {
+    iconName: "weather-snowy",
+    gradient: ["#7DE2FC", "#B9B6E5"],
+    title: "Cloudy",
+    subtitle: "that's cool",
+  },
+  Clear: {
+    iconName: "weather-sunny",
+    gradient: ["#FF7300", "#FEF253"],
+    title: "Cloudy",
+    subtitle: "that's cool",
+  },
+  Clouds: {
+    iconName: "weather-cloudy",
+    gradient: ["#D7D2CC", "#304352"],
+    title: "Cloudy",
+    subtitle: "that's cool",
+  },
+  Haze: {
+    iconName: "weather-hazy",
+    gradient: ["#4DA0B0", "#D39D38"],
+    title: "Cloudy",
+    subtitle: "that's cool",
+  },
+  Mist: {
+    iconName: "weather-fog",
+    gradient: ["#215f00", "#e4e4d9"],
+    title: "Cloudy",
+    subtitle: "that's cool",
+  },
+  Dust: {
+    iconName: "weather-fog",
+    gradient: ["#decba4", "#3e5151"],
+    title: "Cloudy",
+    subtitle: "that's cool",
+  },
+  Fog: {
+    iconName: "weather-fog",
+    gradient: ["#d7d2cc", "#304352"],
+    title: "Cloudy",
+    subtitle: "that's cool",
+  },
+  Tornado: {
+    iconName: "weather-tornado",
+    gradient: ["#525252", "#3d72b4"],
+    title: "Cloudy",
+    subtitle: "that's cool",
+  },
+};
+
+export default function Weather({ temp, condition }) {
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      colors={weatherOptions[condition].gradient}
+      style={styles.container}
+    >
+      <StatusBar style="auto" />
       <View style={styles.halfContainer}>
-        <MaterialCommunityIcons name="weather-cloudy" size={96} color="black" />
-        <Text style={styles.temp}>{temp}°</Text>
+        <MaterialCommunityIcons
+          name={weatherOptions[condition].iconName}
+          size={96}
+          color="white"
+        />
+        <Text style={styles.temp}>{Math.floor(temp)}°</Text>
       </View>
-      <View style={styles.halfContainer}></View>
-    </View>
+      <View style={{ ...styles.halfContainer, ...styles.textContainer }}>
+        <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherOptions[condition].subtitle}
+        </Text>
+      </View>
+    </LinearGradient>
   );
 }
 
@@ -22,12 +105,13 @@ Weather.propTypes = {
     "Drizzle",
     "Rain",
     "Snow",
-    "Atmosphere",
     "Clear",
     "Clouds",
     "Haze",
     "Mist",
     "Dust",
+    "Fog",
+    "Tornado",
   ]).isRequired,
 };
 
@@ -44,5 +128,21 @@ const styles = StyleSheet.create({
   },
   temp: {
     fontSize: 42,
+    color: "#fff",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 44,
+    fontWeight: "300",
+    marginBottom: 10,
+  },
+  subtitle: {
+    color: "#fff",
+    fontWeight: "600",
+    fontSize: 24,
+  },
+  textContainer: {
+    paddingHorizontal: 20,
+    alignItems: "flex-start",
   },
 });
